d186812174f63a0248177ab2ea0b6bc808908615
kenewjr <kenelewatan@gmail.com>
2022-04-29 20:21:36 +0700

    Initial commit

---
+++ app/src/main/java/and5/abrar/mvpnews/adapter/AdapterNEws.kt
@@ -0,0 +1,21 @@
+package and5.abrar.mvpnews.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class AdapterNEws():RecyclerView.Adapter<AdapterNEws.ViewHolder>() {
+    class ViewHolder(itemView : View):RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/main/java/and5/abrar/mvpnews/network/ApiService.kt
@@ -0,0 +1,4 @@
+package and5.abrar.mvpnews.network
+
+interface ApiService {
+}
